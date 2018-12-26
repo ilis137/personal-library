f29cf4841373c6197609a5bda2ac41981b6d2a80
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-12-26 08:06:09 +0530

    Register and login users

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,11 +1,49 @@
+const passport = require("passport")
+const Account = require("../models/Accounts")
+const library = require("../models/Library")
 module.exports = (app) => {
 
     app.get("/", (req, res) => {
         res.send("welcome to Personal Library")
     })
+    app.get("/login", (req, res) => {
+        res.send("login page")
+    })
+    app.post("/login", passport.authenticate("local", { successRedirect: "/Profile", failureRedirect: "/register" }))
 
-    app.post("/login", (req, res) => {
-
+    app.get("/register", (req, res) => {
+        res.send("register page")
     })
 
+    app.post("/register", (req, res) => {
+
+        Account.register(new Account({ username: req.body.username, password: req.body.password }), req.body.password, (err, account) => {
+            if (err) {
+                return console.log("render register faliure page " + err)
+            }
+
+        })
+
+        //passport.authenticate("local", { successRedirect: "/profile", failureRedirect: "/login" })
+        res.redirect("/login")
+    })
+
+    const authCheck = (req, res, next) => {
+            //console.log(req.user)
+            if (!req.user) {
+                res.redirect("/register")
+            } else {
+                next()
+            }
+        }
+        //if session established profile can be accessed after one login
+    router.get("/", authCheck, (req, res) => {
+
+        res.send("profilepage")
+    })
+
+    app.get('/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
 }
