29becb5d916ee1b183d6db1dd94727b9c45ce286
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-12-28 08:12:34 +0530

    register and authenticate user

--- Routes/routes.js
+++ Routes/routes.js
@@ -7,25 +7,28 @@ module.exports = (app) => {
         res.render("home")
     })
     app.get("/login", (req, res) => {
-        res.send("login page")
+        res.render("login")
     })
-    app.post("/login", passport.authenticate("local", { successRedirect: "/Profile/api/books", failureRedirect: "/register" }))
+    app.post("/login", passport.authenticate("local", { successRedirect: "/Profile/api/books", failureRedirect: "/register" }), () => {
+        console.log("in login")
+    })
 
     app.get("/register", (req, res) => {
-        res.send("register page")
+        res.render("register")
     })
 
     app.post("/register", (req, res) => {
-
+        console.log(req.body.username)
         Account.register(new Account({ username: req.body.username, password: req.body.password }), req.body.password, (err, account) => {
             if (err) {
-                return console.log("render register faliure page " + err)
+                console.log("render register faliure page " + err)
+                return res.render("register", { error: err })
             }
-
+            res.redirect("/login")
         })
 
         //passport.authenticate("local", { successRedirect: "/profile", failureRedirect: "/login" })
-        res.redirect("/login")
+
     })
 
     const authCheck = (req, res, next) => {
@@ -44,6 +47,7 @@ module.exports = (app) => {
         library.find(user).then((doc) => {
             if (!doc)
                 return res.status(404).send()
+
             res.send({ doc })
         }).catch((err) => {
             res.status(400).send()
