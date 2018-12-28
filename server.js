53a7294eecd402737e08bb61c4cab9df75fac728
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-12-28 08:12:56 +0530

    add handlebars middleware

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const passport = require("passport")
 const LocalStrategy = require("passport-local").Strategy
 const mongoose = require("mongoose")
 const cookieSession = require("cookie-session")
-    //const router = require("express").Router()
-
+const hbs = require("hbs")
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-//app.use("/profile", router)
+hbs.registerPartials(__dirname + "/views/partials")
+app.set('view engine', 'hbs');
 app.use(cors())
 app.use(cookieSession({ maxAge: 24 * 60 * 60 * 1000, keys: ["sdfgghhvcde"] }))
 app.use(require("body-parser").json())
@@ -19,6 +19,10 @@ app.use(require("body-parser").urlencoded({ extended: true }))
 app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/node_modules/bootstrap/dist"))
+
+
 
 const Account = require("./models/Accounts")
 passport.use(new LocalStrategy(
@@ -28,7 +32,7 @@ passport.serializeUser(Account.serializeUser())
 passport.deserializeUser(Account.deserializeUser())
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb://localhost:27017/ShortURL", { useNewUrlParser: true })
+mongoose.connect("mongodb://localhost:27017/personallibrary", { useNewUrlParser: true })
 
 require("./routes/routes")(app)
 
